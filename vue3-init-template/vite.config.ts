3de94d504d6303b2fbdd89c2d0910d4b6b9ac95d
aiden <danyang.aiden@outlook.com>
2022-11-14 20:41:35 +0800

    环境配置

--- vue3-init-template/vite.config.ts
+++ vue3-init-template/vite.config.ts
@@ -5,19 +5,30 @@ import { createSvg } from './src/utils/svg'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    vue(),
+    createSvg('./src/assets/icons/'),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src')
     },
     extensions: [".vue", ".js", ".json", ".ts"]
   },
+  server: {
+    host: '0.0.0.0',
+    port: 8300,
+    proxy: {
+      '/api': {
+        target: 'xxx',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   define: {
     __VUE_I18N_FULL_INSTALL__: true,
     __VUE_I18N_LEGACY_API__: false,
     __INTLIFY_PROD_DEVTOOLS__: false,
-  },
-  plugins: [
-    vue(),
-    createSvg('./src/assets/icons/'),
-  ]
+  }
 })
